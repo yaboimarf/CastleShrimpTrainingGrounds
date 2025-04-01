dd2f954fa62356c014df902938f45b22958ce09e
Finn <97118090@st.deltion.nl>
2025-04-01 13:31:30 +0200

    save load changes 2.1

--- CastleShrimpTrainingGrounds/Assets/Code/CodeFinn/Load.cs
+++ CastleShrimpTrainingGrounds/Assets/Code/CodeFinn/Load.cs
@@ -6,45 +6,47 @@ using UnityEngine.UI;
 
 public class Load : MonoBehaviour
 {
-    private string savePath;
-    public Button loadButton;
-    private PlayerBehavior player;
+    private string savePath; 
+    public Button loadButton;  
+    private PlayerBehavior player;  // Een privé variabele om de PlayerBehavior-component van "Kevin" op te slaan
 
-    [System.Serializable]
-    private class PlayerData
+    [System.Serializable]  // Maakt de klasse serialiseerbaar zodat Unity deze kan omzetten van/naar JSON
+    private class PlayerData  // Een privéklasse binnen Load om spelergegevens te structureren
     {
-        public float[] position = new float[3];
-        public float health;
+        public float[] position = new float[3];  // Een array van 3 floats voor de positie (x, y, z)
+        public float health;  // Een float om de gezondheid van de speler te bewaren
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        savePath = Path.Combine(Application.persistentDataPath, "kevinData.json");
-        player = GameObject.Find("Kevin").GetComponent<PlayerBehavior>();
 
-        if (loadButton != null)
+    // Start is called before the first frame update
+    void Start()  // Deze methode wordt uitgevoerd als het script voor het eerst start in Unity
+    {
+        savePath = Path.Combine(Application.persistentDataPath, "kevinData.json");  // Maakt een bestandspad naar "kevinData.json" in een permanente map
+        player = GameObject.Find("Kevin").GetComponent<PlayerBehavior>();  // Zoekt een GameObject genaamd "Kevin" en haalt zijn PlayerBehavior-component op
+
+        if (loadButton != null)  // Controleert of de loadButton is ingesteld (niet null)
         {
-            loadButton.onClick.AddListener(LoadGame);
+            loadButton.onClick.AddListener(LoadGame);  // Voegt de LoadGame-methode toe als actie wanneer de knop wordt geklikt
         }
     }
-    void LoadGame()
-    {
-        if (File.Exists(savePath) && player != null)
-        {
-            string json = File.ReadAllText(savePath);
-            PlayerData data = JsonUtility.FromJson<PlayerData>(json);
 
-            Vector3 position = new Vector3(
-                data.position[0],
-                data.position[1],
-                data.position[2]
+    void LoadGame() 
+    {
+        if (File.Exists(savePath) && player != null)  // Controleert of het bestand bestaat en de speler is gevonden
+        {
+            string json = File.ReadAllText(savePath);  // Leest de volledige tekst (JSON) uit het bestand op savePath
+            PlayerData data = JsonUtility.FromJson<PlayerData>(json);  // Converteert de JSON-string terug naar een PlayerData-object
+
+            Vector3 position = new Vector3(  // Maakt een nieuwe Vector3 voor de positie van de speler
+                data.position[0],  // x-coördinaat uit de geladen data
+                data.position[1],  // y-coördinaat uit de geladen data
+                data.position[2]   // z-coördinaat uit de geladen data
             );
 
-            player.transform.position = position;
-            player.hp = data.health;
-            Debug.Log("Kevin's data loaded!");
+            player.transform.position = position;  // Stelt de positie van "Kevin" in op de geladen positie
+            player.hp = data.health;  // Stelt de gezondheid van "Kevin" in op de geladen gezondheid
+            Debug.Log("Kevin's data loaded!");  // Geeft een bericht in de Unity-console dat de gegevens zijn geladen
         }
-        else
+        else 
         {
             Debug.Log("No save file found or player not found!");
         }
--- CastleShrimpTrainingGrounds/Assets/Code/CodeFinn/Save.cs
+++ CastleShrimpTrainingGrounds/Assets/Code/CodeFinn/Save.cs
@@ -12,39 +12,40 @@ public class Save : MonoBehaviour
 
     [System.Serializable]
 
-    private class PlayerData
+    private class PlayerData  // Een privéklasse om gegevens van de speler op te slaan
     {
-        public float[] position = new float[3];
-        public float health;
+        public float[] position = new float[3];  // Een array van 3 floats voor de positie (x, y, z)
+        public float health;  // Een float om de gezondheid van de speler te bewaren
 
-        public PlayerData(Vector3 pos, float hp)
+        public PlayerData(Vector3 pos, float hp)  // Constructor om een nieuwe PlayerData te maken met positie en gezondheid
         {
-            position[0] = pos.x;
-            position[1] = pos.y;
-            position[2] = pos.z;
-            health = hp;
+            position[0] = pos.x;  // De x-coördinaat van de Vector3 wordt opgeslagen in position[0]
+            position[1] = pos.y;  // De y-coördinaat van de Vector3 wordt opgeslagen in position[1]
+            position[2] = pos.z;  // De z-coördinaat van de Vector3 wordt opgeslagen in position[2]
+            health = hp;  // De gezondheidswaarde (hp) wordt opgeslagen in het health-veld
         }
     }
+
     // Start is called before the first frame update
-    void Start()
+    void Start()  // Deze methode wordt uitgevoerd als het script voor het eerst start in Unity
     {
-        savePath = Path.Combine(Application.persistentDataPath, "kevinData.json");
-        player = GameObject.Find("Kevin").GetComponent<PlayerBehavior>();
+        savePath = Path.Combine(Application.persistentDataPath, "kevinData.json");  // Maakt een bestandspad naar "kevinData.json" in een permanente map
+        player = GameObject.Find("Kevin").GetComponent<PlayerBehavior>();  // Zoekt een GameObject genaamd "Kevin" en haalt zijn PlayerBehavior-component op
 
-        if (saveButton != null)
+        if (saveButton != null)  // Controleert of de saveButton is ingesteld (niet null)
         {
-            saveButton.onClick.AddListener(SaveGame);
+            saveButton.onClick.AddListener(SaveGame);  // Voegt de SaveGame-methode toe als actie wanneer de knop wordt geklikt
         }
     }
 
-    void SaveGame()
+    void SaveGame()  // Methode om de spelergegevens op te slaan
     {
-        if (player != null)
+        if (player != null)  // Controleert of de speler is gevonden en niet null is
         {
-            PlayerData data = new PlayerData(player.transform.position, player.hp);
-            string json = JsonUtility.ToJson(data);
-            File.WriteAllText(savePath, json);
-            Debug.Log("Kevin's data saved!");
+            PlayerData data = new PlayerData(player.transform.position, player.hp);  // Maakt een nieuwe PlayerData met de huidige positie en gezondheid van de speler
+            string json = JsonUtility.ToJson(data);  // Converteert de PlayerData naar een JSON-string
+            File.WriteAllText(savePath, json);  // Schrijft de JSON-string naar het bestand op het savePath
+            Debug.Log("Kevin's data saved!");  // Geeft een bericht in de Unity-console dat de gegevens zijn opgeslagen
         }
     }
 
